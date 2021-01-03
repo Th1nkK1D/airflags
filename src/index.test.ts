031bf79d8a146536abaa6269efab1c0760c6363d
Th1nkK1D <witheep@gmail.com>
2021-01-03 18:46:56 +0700

    Implement Airflags class with basic fetch

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,3 +1,42 @@
-test('Test jest', () => {
-  expect(1 + 1).toBe(2);
+import { server } from './mock/msw';
+import Airflags from '.';
+
+beforeAll(() => server.listen());
+afterAll(() => server.close());
+
+const mockAirtableConfig = {
+  baseId: 'someBase',
+  tableName: 'someTable',
+  apiKey: 'apiKey',
+};
+test('getFlags function should return correct development flags', async () => {
+  const expectedFlags = {
+    featureA: true,
+    featureB: true,
+  };
+
+  const airflags = new Airflags({
+    environment: 'Development',
+    ...mockAirtableConfig,
+  });
+
+  const flags = await airflags.getFlags();
+
+  expect(flags).toStrictEqual(expectedFlags);
+});
+
+test('getFlags function should return correct production flags', async () => {
+  const expectedFlags = {
+    featureA: false,
+    featureB: true,
+  };
+
+  const airflags = new Airflags({
+    environment: 'Production',
+    ...mockAirtableConfig,
+  });
+
+  const flags = await airflags.getFlags();
+
+  expect(flags).toStrictEqual(expectedFlags);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,41 @@
+import { AirtableConfig, fetchAirtableRecords } from './airtable';
+
+interface AirflagsConfig extends AirtableConfig {
+  environment: string;
+}
+
+type FeatureFlags = {
+  [feature: string]: boolean;
+};
+
 class Airflags {
-  private hello() {
-    console.log('Hello');
+  private airtableConfig: AirtableConfig;
+  private environment: string;
+  private featureFlags: FeatureFlags | undefined;
+
+  constructor({ environment, ...airtableConfig }: AirflagsConfig) {
+    this.airtableConfig = airtableConfig;
+    this.environment = environment;
+  }
+
+  private async fetch() {
+    const { records } = await fetchAirtableRecords(this.airtableConfig);
+
+    this.featureFlags = records.reduce(
+      (flags, { fields }) => ({
+        ...flags,
+        [fields.Name]: !!fields[this.environment],
+      }),
+      {}
+    );
+  }
+
+  async getFlags(): Promise<FeatureFlags> {
+    if (!this.featureFlags) {
+      await this.fetch();
+    }
+
+    return this.featureFlags as FeatureFlags;
   }
 }
 
